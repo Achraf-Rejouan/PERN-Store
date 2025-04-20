c3cce8f232af9c562df9a1ad0666441b7d963a72
achrafRejouan <achrafrejouan@gmail.com>
2025-04-20 16:10:15 +0100

    feat: initialize Express server middleware

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json()); // Parse JSON bodies (as sent by API clients)
+app.use(cors()); // Enable CORS for all routes
+app.use(helmet()); // Use Helmet to set various HTTP headers for security
+app.use(morgan('dev')); // Use Morgan for logging HTTP requests in development mode
+
+app.get('/test', (req, res) => {
+    console.log(res.getHeaders());
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
